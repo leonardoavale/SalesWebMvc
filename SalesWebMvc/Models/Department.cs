c51e1205f1c9fb9d8f617e8f245ccbd84dc8d984
leonardoavale <leonardoaguiardovale@gmail.com>
2021-04-04 10:52:22 -0300

    Other Entities and second Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,45 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        /*public double TotalSales(DateTime initial, DateTime final)
+        {
+            double sales = 0;
+            foreach (Seller s in Sellers)
+            {
+                sales = +s.TotalSales(initial, final);
+            }
+            return sales;
+
+        }*/
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
